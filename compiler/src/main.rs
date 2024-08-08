28e89bf246efe3061c04c0cb644794ff336e465b
spineda2019 <spineda.wpi.alum@gmail.com>
2024-08-07 22:30:03 -0400

    depend on argparse (for now)

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,3 +1,19 @@
+use std::path::PathBuf;
+
+use argparse::{ArgumentParser, Store};
+
 fn main() {
-    println!("Hello, world!");
+    let mut file_path: PathBuf = PathBuf::new();
+    {
+        let mut argument_parse: ArgumentParser = ArgumentParser::new();
+        argument_parse.set_description("The Fragment Language Compiler");
+
+        argument_parse
+            .refer(&mut file_path)
+            .add_argument("filename", Store, "File to compile");
+
+        argument_parse.parse_args_or_exit();
+    }
+
+    println!("Compiling: {:?}", file_path);
 }
