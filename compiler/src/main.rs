b7a2ae8e322232de97624a614caf1ca5451e4d3b
spineda2019 <spineda.wpi.alum@gmail.com>
2024-08-08 17:47:08 -0400

    switch to lexer owning pathbuf

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, path::PathBuf};
+use std::path::PathBuf;
 
 use argparse::{ArgumentParser, Collect};
 use common::error::CompilerError;
@@ -21,17 +21,13 @@ fn main() -> Result<(), CompilerError> {
 
     if files.is_empty() {
         println!("Welcome to the Fragment REPL!");
-        lexer.lex();
+        lexer.lex()?;
     } else {
         println!("Compiling files:");
-        for file in files.iter() {
+        for file in files {
             println!("{:?} ", file);
-            let file: File = match File::open(file) {
-                Ok(f) => f,
-                Err(e) => return Err(CompilerError::FileIOError(file.to_path_buf(), e)),
-            };
-            lexer.new_file(file);
-            lexer.lex();
+            lexer.new_file(file)?;
+            lexer.lex()?;
         }
     }
 
--- lexer/src/lexer.rs
+++ lexer/src/lexer.rs
@@ -1,30 +1,86 @@
 use std::{
     fs::File,
-    io::{stdin, stdout, Write},
+    io::{stdin, stdout, BufRead, BufReader, Write},
+    path::PathBuf,
 };
 
-use common::token::Token;
+use common::{error::CompilerError, token::Token};
 
 pub struct Lexer {
-    current_file: Option<File>,
+    current_file: Option<PathBuf>,
+    current_line_number: usize,
 }
 
 impl Lexer {
     pub fn init() -> Lexer {
-        Lexer { current_file: None }
+        Lexer {
+            current_file: None,
+            current_line_number: 0,
+        }
     }
 
-    pub fn new_file(&mut self, file: File) {
+    pub fn new_file(&mut self, file: PathBuf) -> Result<(), CompilerError> {
+        match file.extension() {
+            None => return Err(CompilerError::UnrecognizedFileError(file)),
+            Some(extension) => {
+                match extension.to_str() {
+                    Some("fr") => {}
+                    _ => return Err(CompilerError::UnrecognizedFileError(file)),
+                };
+            }
+        };
+
         self.current_file = Some(file);
+        self.current_line_number = 0;
+        Ok(())
     }
 
-    fn get_token(&self) -> Token {
-        todo!()
+    fn lex_file(&mut self) -> Result<Vec<Token>, CompilerError> {
+        let file: Result<File, _> = match &self.current_file {
+            Some(f) => File::open(f),
+            None => return Err(CompilerError::NonExistentFileError),
+        };
+        let file: File = match file {
+            Ok(f) => f,
+            Err(e) => {
+                return Err(CompilerError::FileIOError(
+                    self.current_file
+                        .clone()
+                        .unwrap_or("Could not get file info...".into()),
+                    e,
+                ))
+            }
+        };
+
+        let tokens: Vec<Token> = Vec::new();
+        let mut last_char: char = ' ';
+
+        let reader: BufReader<File> = BufReader::new(file);
+        for line in reader.lines() {
+            let line = match line {
+                Ok(l) => l,
+                Err(e) => {
+                    return Err(CompilerError::UnreadableLineError(
+                        self.current_file
+                            .clone()
+                            .unwrap_or("Could not get file info...".into()),
+                        self.current_line_number,
+                        e,
+                    ))
+                }
+            };
+
+            self.current_line_number += 1;
+
+            for c in line.trim().chars() {
+                last_char = c;
+            }
+        }
+
+        Ok(tokens)
     }
 
-    fn lex_file(&self) {}
-
-    fn lex_stdin(&self) {
+    fn lex_stdin(&self) -> Result<Vec<Token>, CompilerError> {
         let mut line: String = String::new();
         loop {
             print!("Fragment REPL >> ");
@@ -36,7 +92,7 @@ impl Lexer {
         }
     }
 
-    pub fn lex(&self) {
+    pub fn lex(&mut self) -> Result<Vec<Token>, CompilerError> {
         match self.current_file {
             Some(_) => self.lex_file(),
             None => self.lex_stdin(),
