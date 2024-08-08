4d0e6f25c07cda317e00f1414bcf0dc6cb4ce96e
spineda2019 <spineda.wpi.alum@gmail.com>
2024-08-07 22:44:06 -0400

    improve arg collection

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -1,19 +1,22 @@
 use std::path::PathBuf;
 
-use argparse::{ArgumentParser, Store};
+use argparse::{ArgumentParser, Collect};
 
 fn main() {
-    let mut file_path: PathBuf = PathBuf::new();
+    let mut files: Vec<PathBuf> = Vec::new();
     {
         let mut argument_parse: ArgumentParser = ArgumentParser::new();
         argument_parse.set_description("The Fragment Language Compiler");
 
         argument_parse
-            .refer(&mut file_path)
-            .add_argument("filename", Store, "File to compile");
+            .refer(&mut files)
+            .add_argument("filename", Collect, "File to compile");
 
         argument_parse.parse_args_or_exit();
     }
 
-    println!("Compiling: {:?}", file_path);
+    println!("Compiling files:");
+    for file in files {
+        print!("{:?} ", file);
+    }
 }
