025f014876076a2e72ffa34dd96f904e195fd99b
spineda2019 <spineda.wpi.alum@gmail.com>
2024-08-11 00:22:11 -0400

    fix conditional compilation

--- lexer/src/char_reader.rs
+++ lexer/src/char_reader.rs
@@ -53,7 +53,7 @@ mod charreader_tests {
         {
             expected = "I am a group of chars\r\nthat should be consumable\r\n";
         }
-        #[cfg(not(target_os = ("windows")))]
+        #[cfg(not(target_os = "windows"))]
         {
             expected = "I am a group of chars\nthat should be consumable\n";
         }
