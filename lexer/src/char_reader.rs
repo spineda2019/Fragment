eca858c9f57e626d26eb12ac95d2a86513ceb9c5
spineda2019 <spineda.wpi.alum@gmail.com>
2024-08-11 10:42:33 -0400

    move getchar method to iterator

--- lexer/src/char_reader.rs
+++ lexer/src/char_reader.rs
@@ -22,7 +22,7 @@ impl CharReader {
         })
     }
 
-    pub fn getchar(&mut self) -> Option<char> {
+    fn getchar(&mut self) -> Option<char> {
         if let Some(c) = self.file_map.get(self.byte_pointer) {
             self.byte_pointer += 1;
             let character: char = char::from(*c);
@@ -33,13 +33,17 @@ impl CharReader {
     }
 }
 
+impl Iterator for CharReader {
+    type Item = char;
+
+    fn next(&mut self) -> Option<char> {
+        self.getchar()
+    }
+}
+
 #[cfg(test)]
 mod charreader_tests {
-    use std::{
-        env::current_dir,
-        fs::{self, File},
-        path::PathBuf,
-    };
+    use std::{env::current_dir, fs::File, path::PathBuf};
 
     use common::error::CompilerError;
 
@@ -85,6 +89,50 @@ mod charreader_tests {
         let mut result: String = String::with_capacity(expected.len());
 
         while let Some(c) = reader.getchar() {
+            result.push(c);
+        }
+
+        assert_eq!(expected, &result);
+    }
+
+    #[test]
+    fn test_iterator() {
+        let expected: &str;
+
+        #[cfg(target_os = "windows")]
+        {
+            expected = "I am a group of chars\r\nthat should be consumable\r\n";
+        }
+        #[cfg(not(target_os = "windows"))]
+        {
+            expected = "I am a group of chars\nthat should be consumable\n";
+        }
+
+        let current_dir: Result<PathBuf, _> = current_dir();
+        assert!(current_dir.is_ok());
+        let mut current_dir: PathBuf = current_dir.unwrap();
+
+        let prev: PathBuf = PathBuf::from("..");
+        let test_dir: PathBuf = PathBuf::from("test_utils");
+        let test_file: PathBuf = PathBuf::from("char_reader.txt");
+
+        current_dir.push(prev);
+        current_dir.push(test_dir);
+        current_dir.push(test_file);
+
+        let file: Result<File, _> = File::open(current_dir.clone());
+        assert!(file.is_ok());
+
+        let file: File = file.unwrap();
+        let reader: Result<CharReader, CompilerError> = CharReader::new(&file, &current_dir);
+
+        assert!(reader.is_ok());
+
+        let reader: CharReader = reader.unwrap();
+
+        let mut result: String = String::new();
+
+        for c in reader {
             result.push(c);
         }
 
