da1bd8ebb999a4879039410a3fba3288c59b713b
spineda2019 <spineda.wpi.alum@gmail.com>
2024-08-24 17:33:21 -0400

    complete token lexing

--- lexer/src/lexer.rs
+++ lexer/src/lexer.rs
@@ -11,6 +11,7 @@ use crate::char_reader::CharReader;
 pub struct Lexer {
     current_file: Option<PathBuf>,
     current_line_number: usize,
+    current_character_position: usize,
 }
 
 impl Lexer {
@@ -18,6 +19,7 @@ impl Lexer {
         Lexer {
             current_file: None,
             current_line_number: 0,
+            current_character_position: 0,
         }
     }
 
@@ -35,6 +37,97 @@ impl Lexer {
         self.current_file = Some(file);
         self.current_line_number = 0;
         Ok(())
+    }
+
+    fn collect_number(
+        &mut self,
+        reader: &mut CharReader,
+        built_lexeme: &mut String,
+    ) -> Result<Token, CompilerError> {
+        while let Some(c) = reader.getchar() {
+            if c.is_ascii_digit() || c == '.' {
+                built_lexeme.push(c);
+            } else {
+                break;
+            }
+        }
+
+        match built_lexeme.parse::<i64>() {
+            Ok(n) => Ok(Token::F64Literal(n)),
+            Err(_) => todo!(),
+        }
+    }
+
+    fn collect_identifier(
+        &mut self,
+        reader: &mut CharReader,
+        built_lexeme: &mut String,
+    ) -> Result<Token, CompilerError> {
+        while let Some(c) = reader.getchar() {
+            if c.is_ascii_alphabetic() {
+                built_lexeme.push(c);
+                continue;
+            } else {
+                break;
+            }
+        }
+
+        return match built_lexeme.as_str() {
+            "def" => Ok(Token::Def),
+            "extern" => Ok(Token::Extern),
+            _ => Ok(Token::Identifier(built_lexeme.to_string())),
+        };
+    }
+
+    fn get_token(&mut self, reader: &mut CharReader) -> Result<Token, CompilerError> {
+        let mut last_char: char = ' ';
+
+        loop {
+            match reader.getchar() {
+                Some(c) if c.is_ascii_whitespace() => {
+                    if c == '\n' {
+                        self.current_line_number += 1;
+                    }
+                    last_char = c;
+                    continue;
+                }
+                Some(notspace) => {
+                    last_char = notspace;
+                    break;
+                }
+                None => {
+                    return Ok(Token::Eof);
+                }
+            };
+        }
+
+        let mut built_lexeme = String::new();
+
+        match last_char {
+            character if character.is_ascii_alphabetic() => {
+                built_lexeme.push(character);
+                self.collect_identifier(reader, &mut built_lexeme)
+            }
+            digit if digit.is_ascii_digit() || digit == '.' => {
+                built_lexeme.push(digit);
+                self.collect_number(reader, &mut built_lexeme)
+            }
+            '#' => {
+                while let Some(c) = reader.getchar() {
+                    if c == '\n' || c == '\r' {
+                        self.current_line_number += 1;
+                        return self.get_token(reader);
+                    }
+                }
+
+                Ok(Token::Eof)
+            }
+            '+' => Ok(Token::PlusOperator),
+            '-' => Ok(Token::MinusOperator),
+            '*' => Ok(Token::MultiplicationOperator),
+            '/' => Ok(Token::DivisionOperator),
+            unknown => Ok(Token::Unknown(unknown)),
+        }
     }
 
     fn lex_file(&mut self) -> Result<Vec<Token>, CompilerError> {
@@ -56,14 +149,19 @@ impl Lexer {
 
         let mut char_reader: CharReader = CharReader::new(&file, &current_path)?;
 
-        let tokens: Vec<Token> = Vec::new();
-        let mut last_char: char = ' ';
-        let mut built_lexeme: String = String::new();
-
-        for c in char_reader {
-            while c.is_ascii_whitespace() {
-                continue;
-            }
+        let mut tokens: Vec<Token> = Vec::new();
+        loop {
+            let token: Token = self.get_token(&mut char_reader)?;
+            match token {
+                Token::Eof => {
+                    tokens.push(token);
+                    break;
+                }
+                _ => {
+                    tokens.push(token);
+                    continue;
+                }
+            };
         }
 
         Ok(tokens)
