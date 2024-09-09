46aec1593d1acc2197388db5a91ddc51be8f94cc
spineda2019 <spineda.wpi.alum@gmail.com>
2024-09-08 21:24:05 -0400

    impl traits for function nodes

--- parser/src/ast_nodes/functions/function_definition.rs
+++ parser/src/ast_nodes/functions/function_definition.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use crate::ast_node::ASTNode;
 
 use super::function_prototype::FunctionPrototype;
@@ -13,4 +15,20 @@ impl Function {
     }
 }
 
-impl ASTNode for Function {}
+impl Display for Function {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Function Prototype: {}\nFunction Body: {}",
+            self.prototype, self.body
+        )
+    }
+}
+
+impl ASTNode for Function {
+    fn print(&self) {
+        println!("Node: Function");
+        println!("Prototype: {}", self.prototype);
+        println!("body: {}", self.body);
+    }
+}
--- parser/src/ast_nodes/functions/function_prototype.rs
+++ parser/src/ast_nodes/functions/function_prototype.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use crate::ast_node::ASTNode;
 
 pub struct FunctionPrototype {
@@ -14,4 +16,20 @@ impl FunctionPrototype {
     }
 }
 
-impl ASTNode for FunctionPrototype {}
+impl Display for FunctionPrototype {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let display: String = format!(
+            "Function Prototype Name: {}\nFunction Prototype Args: {:?}",
+            self.name, self.args
+        );
+        write!(f, "{}", display)
+    }
+}
+
+impl ASTNode for FunctionPrototype {
+    fn print(&self) {
+        println!("Node: Function Prototype");
+        println!("Function Prototype: {}", self.name);
+        println!("Function args: {:?}", self.args);
+    }
+}
